db55e681fb045b76a645b5448b8d02bf4ea827df
daviaxs <cadwvk@gmail.com>
2024-08-27 16:14:58 -0300

    feat: Add lucide icons dependencies and add LogOut icon in HomeHeader

--- src/shared/components/home-header/HomeHeader.tsx
+++ src/shared/components/home-header/HomeHeader.tsx
@@ -1,21 +1,24 @@
 import { UserPhoto } from "@components/user-photo/UserPhoto";
-import { Heading, HStack, Text, VStack } from "@gluestack-ui/themed";
+import { Heading, HStack, Icon, Text, VStack } from "@gluestack-ui/themed";
+import { LogOut } from "lucide-react-native";
 
 export function HomeHeader() {
   return (
     <HStack bg="$gray600" pt="$16" pb="$5" px="$8" alignItems="center" gap="$4">
-      <UserPhoto 
-        source={{uri: "https://github.com/daviaxs.png"}}
+      <UserPhoto
+        source={{ uri: "https://github.com/daviaxs.png" }}
         alt="Imagem do usuário"
         h="$16"
         w="$16"
       />
 
-      <VStack>
+      <VStack flex={1}>
         <Text color="$gray100" fontSize="$sm">Olá</Text>
 
         <Heading color="$gray100" fontSize="$md">Davi Alves</Heading>
       </VStack>
+
+      <Icon as={LogOut} color="$gray200" size="xl" />
     </HStack>
   )
 }
