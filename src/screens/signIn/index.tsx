ad67469845211335b521e46fa167cfc53021f8a8
daviaxs <cadwvk@gmail.com>
2024-08-27 12:36:27 -0300

    feat: Add Input component for sign-in form

--- src/screens/signIn/index.tsx
+++ src/screens/signIn/index.tsx
@@ -1,6 +1,7 @@
-import { VStack, Image, Center, Text } from '@gluestack-ui/themed'
+import { VStack, Image, Center, Text, Heading } from '@gluestack-ui/themed'
 import BackgroundImg from '@assets/background.png'
 import Logo from '@assets/logo.svg'
+import { Input } from '@components/input/Input'
 
 export function SignInScreen() {
   return (
@@ -14,13 +15,22 @@ export function SignInScreen() {
         position='absolute'
       />
 
-      <Center my="$24">
-        <Logo />
+      <VStack flex={1} px="$10" pb="$16">
+        <Center my="$24">
+          <Logo />
 
-        <Text color='$gray100' fontSize="$sm">
-          Treine sua mente e seu corpo
-        </Text>
-      </Center>
+          <Text color='$gray100' fontSize="$sm">
+            Treine sua mente e seu corpo
+          </Text>
+        </Center>
+
+        <Center gap="$2">
+          <Heading color='$gray100'>Acesse a conta</Heading>
+
+          <Input placeholder='Email' />
+          <Input placeholder='Password' />
+        </Center>
+      </VStack>
     </VStack>
   )
 }
---
+++ src/shared/components/input/Input.tsx
@@ -0,0 +1,20 @@
+import { Input as GluestackInput, InputField } from '@gluestack-ui/themed'
+import { ComponentProps } from 'react'
+
+type InputProps = ComponentProps<typeof InputField>
+
+export function Input({ ...rest }: InputProps) {
+  return (
+    <GluestackInput
+      bg='$gray700'
+      h="$14"
+      px="$4"
+      borderWidth="$0"
+      borderRadius="$md"
+    >
+      <InputField
+        {...rest}
+      />
+    </GluestackInput>
+  )
+}
