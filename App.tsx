e5d968a081110714e1e0570ffe9db7dfe9cdce73
daviaxs <cadwvk@gmail.com>
2024-08-26 21:30:34 -0300

    chore: add gluestack theme config

--- App.tsx
+++ App.tsx
@@ -1,14 +1,15 @@
 import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
+import { Text } from '@gluestack-ui/themed';
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { View } from 'react-native'
 
 export default function App() {
   const [fontsLoaded] = useFonts({Roboto_700Bold, Roboto_400Regular})
 
   return (
-    <View style={{flex: 1, backgroundColor: "#fff", alignItems: "center", justifyContent: "center"}}>
+    <View style={{flex: 1, backgroundColor: "#202024", alignItems: "center", justifyContent: "center"}}>
       <StatusBar style="light" backgroundColor="transparent" translucent />
-      {fontsLoaded ? <Text>Home</Text> : <View />}
+      {fontsLoaded ? <Text color='white'>Home</Text> : <View />}
     </View>
   );
 }
