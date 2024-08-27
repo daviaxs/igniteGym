a0dca6e1f5133dae52932b3228aee548eb74c26f
daviaxs <cadwvk@gmail.com>
2024-08-26 21:47:34 -0300

    chore: add color green in Spinner component

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
-import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
-import { Text } from '@gluestack-ui/themed';
+import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto'
+import { Text } from '@gluestack-ui/themed'
 import { StatusBar } from 'expo-status-bar'
 import { View } from 'react-native'
+import { Loading } from '@components/loading/Loading'
 
 export default function App() {
   const [fontsLoaded] = useFonts({Roboto_700Bold, Roboto_400Regular})
@@ -9,7 +10,7 @@ export default function App() {
   return (
     <View style={{flex: 1, backgroundColor: "#202024", alignItems: "center", justifyContent: "center"}}>
       <StatusBar style="light" backgroundColor="transparent" translucent />
-      {fontsLoaded ? <Text color='white'>Home</Text> : <View />}
+      {fontsLoaded ? <Text color='white'>Home</Text> : <Loading />}
     </View>
   );
 }
