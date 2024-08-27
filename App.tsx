9129773a3763e9e8345ef401358f4be70d81b4e9
daviaxs <cadwvk@gmail.com>
2024-08-26 21:48:18 -0300

    chore: add GluestackUIProvider in App

--- App.tsx
+++ App.tsx
@@ -1,16 +1,22 @@
 import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto'
-import { Text } from '@gluestack-ui/themed'
+import { Center, GluestackUIProvider, Text } from '@gluestack-ui/themed'
 import { StatusBar } from 'expo-status-bar'
-import { View } from 'react-native'
 import { Loading } from '@components/loading/Loading'
+import { config } from './config/gluestack-ui.config'
 
 export default function App() {
-  const [fontsLoaded] = useFonts({Roboto_700Bold, Roboto_400Regular})
+  const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular })
 
   return (
-    <View style={{flex: 1, backgroundColor: "#202024", alignItems: "center", justifyContent: "center"}}>
+    <GluestackUIProvider config={config}>
       <StatusBar style="light" backgroundColor="transparent" translucent />
-      {fontsLoaded ? <Text color='white'>Home</Text> : <Loading />}
-    </View>
+      {fontsLoaded ? (
+        <Center flex={1} bg='$gray700'>
+          <Text color='white'>Home</Text>
+        </Center>
+      ) : (
+        <Loading />
+      )}
+    </GluestackUIProvider>
   );
 }
