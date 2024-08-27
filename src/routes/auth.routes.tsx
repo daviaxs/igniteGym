43c3144240abdec4a996d0c25007357194c9624e
daviaxs <cadwvk@gmail.com>
2024-08-27 14:18:43 -0300

    feat: Add auth routes and screens

---
+++ src/routes/auth.routes.tsx
@@ -0,0 +1,28 @@
+import { createNativeStackNavigator, NativeStackNavigationProp } from "@react-navigation/native-stack"
+import { SignInScreen } from "@screens/signIn"
+import { SignUpScreen } from "@screens/signUp"
+
+type AuthRoutesParams = {
+  sigIn: undefined
+  signUp: undefined
+}
+
+export type AuthNavigatorRoutesProps = NativeStackNavigationProp<AuthRoutesParams>
+
+const { Navigator, Screen } = createNativeStackNavigator<AuthRoutesParams>()
+
+export function AuthRoutes() {
+  return (
+    <Navigator>
+      <Screen
+        name="sigIn"
+        component={SignInScreen}
+      />
+
+      <Screen
+        name="signUp"
+        component={SignUpScreen}
+      />
+    </Navigator>
+  )
+}
