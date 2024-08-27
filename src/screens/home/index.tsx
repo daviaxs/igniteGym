c64e15c29174d7e8775a7bb05c6fff5b5f978c9b
daviaxs <cadwvk@gmail.com>
2024-08-27 20:13:11 -0300

    refactor: Update HomeScreen to use FlatList for group selection

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,29 +1,33 @@
-import { Group } from "@components/group/Group";
-import { HomeHeader } from "@components/home-header/HomeHeader";
-import { HStack, ScrollView, VStack } from "@gluestack-ui/themed";
-import { useState } from "react";
+import { Group } from "@components/group/Group"
+import { HomeHeader } from "@components/home-header/HomeHeader"
+import { VStack } from "@gluestack-ui/themed"
+import { useState } from "react"
+import { FlatList } from "react-native"
 
 export function HomeScreen() {
+  const [groups, setGroups] = useState(['costas', 'bíceps', 'tríceps', 'ombro'])
   const [groupActive, setGroupActive] = useState('costas')
 
   return (
     <VStack flex={1}>
       <HomeHeader />
 
-      <ScrollView horizontal>
-        <HStack gap={10} marginHorizontal="$8" marginVertical="$4">
+      <FlatList
+        horizontal
+        data={groups}
+        keyExtractor={(item) => item}
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ paddingHorizontal: 32 }}
+        style={{ marginVertical: 40, maxHeight: 44, minHeight: 44, gap: 16 }}
+        renderItem={({ item }) => (
           <Group
-            name="costas"
-            isActive={groupActive === 'costas'}
-            onPress={() => setGroupActive('costas')}
+            name={item}
+            isActive={groupActive === item}
+            onPress={() => setGroupActive(item)}
+            style={{ marginRight: 16 }}
           />
-          <Group
-            name="pernas"
-            isActive={groupActive === 'pernas'}
-            onPress={() => setGroupActive('pernas')}
-          />
-        </HStack>
-      </ScrollView>
+        )}
+      />
     </VStack>
   )
 }
