38588f0bb3557d53b402cd60361f314ffb5bf58f
daviaxs <cadwvk@gmail.com>
2024-08-27 15:16:38 -0300

    feat: add icons in TabNavigator

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -4,6 +4,11 @@ import { HistoryScreen } from "@screens/history"
 import { HomeScreen } from "@screens/home"
 import { ProfileScreen } from "@screens/profile"
 
+import HomeSvg from "@assets/home.svg"
+import HistorySvg from "@assets/history.svg"
+import ProfileSvg from "@assets/profile.svg"
+import { gluestackUIConfig } from "@gluestack-ui/config"
+
 type AppRoutesParams = {
   home: undefined
   history: undefined
@@ -16,24 +21,42 @@ export type AppNavigatorRoutesProps = BottomTabNavigationProp<AppRoutesParams>
 const { Navigator, Screen } = createBottomTabNavigator<AppRoutesParams>()
 
 export function AppRoutes() {
+  const { tokens } = gluestackUIConfig
+  const iconSize = tokens.space["6"]
+
   return (
-    <Navigator screenOptions={{ 
+    <Navigator screenOptions={{
       headerShown: false,
       tabBarShowLabel: false
     }}>
       <Screen
         name="home"
         component={HomeScreen}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <HomeSvg fill={color} width={iconSize} height={iconSize} />
+          )
+        }}
       />
 
       <Screen
         name="history"
         component={HistoryScreen}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <HistorySvg fill={color} width={iconSize} height={iconSize} />
+          )
+        }}
       />
 
       <Screen
         name="profile"
         component={ProfileScreen}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <ProfileSvg fill={color} width={iconSize} height={iconSize} />
+          )
+        }}
       />
 
       <Screen
