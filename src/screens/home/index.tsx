6d79b3cdda73156b8c049001c6a1e4a06731d9e2
daviaxs <cadwvk@gmail.com>
2024-08-27 16:35:39 -0300

    feat: add ScrollView horizontal in groups component

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,13 +1,29 @@
 import { Group } from "@components/group/Group";
 import { HomeHeader } from "@components/home-header/HomeHeader";
-import { Center, Text, VStack } from "@gluestack-ui/themed";
+import { HStack, ScrollView, VStack } from "@gluestack-ui/themed";
+import { useState } from "react";
 
 export function HomeScreen() {
+  const [groupActive, setGroupActive] = useState('costas')
+
   return (
     <VStack flex={1}>
       <HomeHeader />
 
-      <Group name="Costas" isActive={false} />
+      <ScrollView horizontal>
+        <HStack gap={10} marginHorizontal="$8" marginVertical="$4">
+          <Group
+            name="costas"
+            isActive={groupActive === 'costas'}
+            onPress={() => setGroupActive('costas')}
+          />
+          <Group
+            name="pernas"
+            isActive={groupActive === 'pernas'}
+            onPress={() => setGroupActive('pernas')}
+          />
+        </HStack>
+      </ScrollView>
     </VStack>
   )
 }
