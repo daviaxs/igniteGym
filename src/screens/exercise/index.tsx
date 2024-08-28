58bb584bcf96eb6f1ae0afafa543b92ddbff3f75
daviaxs <cadwvk@gmail.com>
2024-08-28 14:58:17 -0300

    feat: add heading in Exercise screen

--- src/screens/exercise/index.tsx
+++ src/screens/exercise/index.tsx
@@ -1,9 +1,51 @@
-import { Center, Text } from "@gluestack-ui/themed";
+import { Heading, HStack, Icon, Text, VStack } from "@gluestack-ui/themed"
+import { useNavigation } from "@react-navigation/native"
+import { AppNavigatorRoutesProps } from "@routes/app.routes"
+import { ArrowLeft } from "lucide-react-native"
+import { TouchableOpacity } from "react-native"
+import BodySvg from "@assets/body.svg"
 
 export function ExerciseScreen() {
+  const navigation = useNavigation<AppNavigatorRoutesProps>()
+
+  function handleGoBack() {
+    navigation.goBack()
+  }
+
   return (
-    <Center flex={1}>
-      <Text color="$white">Exercise</Text>
-    </Center>
+    <VStack flex={1}>
+      <VStack px="$8" bg="$gray600" pt="$14" pb="$4">
+        <TouchableOpacity onPress={handleGoBack}>
+          <Icon as={ArrowLeft} color="$green500" size="xl" />
+        </TouchableOpacity>
+
+        <HStack
+          justifyContent="space-between"
+          alignItems="center"
+          mt="$4"
+        >
+          <Heading
+            color="$gray100"
+            fontFamily="$heading"
+            fontSize="$lg"
+            flexShrink={1}
+          >
+            Puxada frontal
+          </Heading>
+
+          <HStack alignItems="center">
+            <BodySvg />
+
+            <Text
+              color="$gray200"
+              ml="$1"
+              textTransform="capitalize"
+            >
+              Costas
+            </Text>
+          </HStack>
+        </HStack>
+      </VStack>
+    </VStack>
   )
 }
