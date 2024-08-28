3ce6b8f6d661afc8e28f4ac02073650906c8d30f
daviaxs <cadwvk@gmail.com>
2024-08-27 21:16:08 -0300

    feat: Add HistoryCard component and use it in HistoryScreen

--- src/screens/history/index.tsx
+++ src/screens/history/index.tsx
@@ -1,10 +1,13 @@
 import { ScreenHeader } from "@components/screen-header/ScreenHeader"
 import { VStack } from "@gluestack-ui/themed"
+import { HistoryCard } from "./components/HistoryCard"
 
 export function HistoryScreen() {
   return (
     <VStack flex={1}>
       <ScreenHeader title="Histórico" />
+
+      <HistoryCard />
     </VStack>
   )
 }
