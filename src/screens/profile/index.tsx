424fafda5d5684b27720edcf7aef00b0a429506b
daviaxs <cadwvk@gmail.com>
2024-08-28 13:51:28 -0300

    feat: Update ProfileScreen with ScreenHeader and UserPhoto components

--- src/screens/profile/index.tsx
+++ src/screens/profile/index.tsx
@@ -1,9 +1,22 @@
-import { Center, Text } from "@gluestack-ui/themed";
+import { ScreenHeader } from "@components/screen-header/ScreenHeader"
+import { UserPhoto } from "@components/user-photo/UserPhoto"
+import { Center, VStack } from "@gluestack-ui/themed"
+import { ScrollView } from "react-native"
 
 export function ProfileScreen() {
   return (
-    <Center flex={1}>
-      <Text color="$white">Profile</Text>
-    </Center>
+    <VStack>
+      <ScreenHeader title="Perfil" />
+
+      <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
+        <Center mt="$6" px="$10">
+          <UserPhoto 
+            source={{ uri: 'https://github.com/daviaxs.png' }}
+            alt="Foto de perfil"
+            size="xl"
+          />
+        </Center>
+      </ScrollView>
+    </VStack>
   )
 }
