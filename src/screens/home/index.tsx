9aeb95f99cd9ca8b938dadbb28928e610c3ec457
daviaxs <cadwvk@gmail.com>
2024-08-31 15:29:26 -0300

    refactor: Add loading state and component to HomeScreen

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,6 +1,7 @@
 import { ExerciseCard } from "@components/exercise-card/ExerciseCard"
 import { Group } from "@components/group/Group"
 import { HomeHeader } from "@components/home-header/HomeHeader"
+import { Loading } from "@components/loading/Loading"
 import { ToastAlert } from "@components/toast-alert/ToastAlert"
 import { Heading, HStack, Text, useToast, VStack } from "@gluestack-ui/themed"
 import { useNavigation } from "@react-navigation/native"
@@ -11,6 +12,7 @@ import { useEffect, useState } from "react"
 import { FlatList } from "react-native"
 
 export function HomeScreen() {
+  const [isLoading, setIsLoading] = useState(false)
   const [groups, setGroups] = useState<string[]>([])
   const [groupActive, setGroupActive] = useState('costas')
   const [exercises, setExercises] = useState<string[]>([])
@@ -25,6 +27,7 @@ export function HomeScreen() {
 
   async function fetchGroups() {
     try {
+      setIsLoading(true)
       const response = await api.get('/groups')
       setGroups(response.data)
     } catch (error) {
@@ -32,6 +35,8 @@ export function HomeScreen() {
       const message = isAppError ? error.message : 'Não foi possível carregar os grupos musculares.'
 
       ToastAlert({ message, toast })
+    } finally {
+      setIsLoading(false)
     }
   }
 
@@ -43,44 +48,51 @@ export function HomeScreen() {
     <VStack flex={1}>
       <HomeHeader />
 
-      <FlatList
-        horizontal
-        data={groups}
-        keyExtractor={(item) => item}
-        showsHorizontalScrollIndicator={false}
-        contentContainerStyle={{ paddingHorizontal: 32 }}
-        style={{ marginVertical: 40, maxHeight: 44, minHeight: 44 }}
-        renderItem={({ item }) => (
-          <Group
-            name={item}
-            isActive={groupActive.toLowerCase() === item.toLowerCase()}
-            onPress={() => setGroupActive(item)}
-            style={{ marginRight: 16 }}
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <>
+          <FlatList
+            horizontal
+            data={groups}
+            keyExtractor={(item) => item}
+            showsHorizontalScrollIndicator={false}
+            contentContainerStyle={{ paddingHorizontal: 32 }}
+            style={{ marginVertical: 40, maxHeight: 44, minHeight: 44 }}
+            renderItem={({ item }) => (
+              <Group
+                name={item}
+                isActive={groupActive.toLowerCase() === item.toLowerCase()}
+                onPress={() => setGroupActive(item)}
+                style={{ marginRight: 16 }}
+              />
+            )}
           />
-        )}
-      />
 
-      <VStack px="$8" flex={1}>
-        <HStack justifyContent="space-between" alignItems="center" marginBottom="$8">
-          <Heading color="$gray200" fontFamily="$body">
-            Exercícios
-          </Heading>
+          <VStack px="$8" flex={1}>
+            <HStack justifyContent="space-between" alignItems="center" marginBottom="$8">
+              <Heading color="$gray200" fontFamily="$body">
+                Exercícios
+              </Heading>
 
-          <Text>
-            {exercises.length}
-          </Text>
-        </HStack>
+              <Text>
+                {exercises.length}
+              </Text>
+            </HStack>
 
-        <FlatList
-          data={exercises}
-          keyExtractor={(_, index) => String(index)}
-          showsVerticalScrollIndicator={false}
-          renderItem={({ item }) => (
-            <ExerciseCard name={item} onPress={handleOpenExerciseDetails} />
-          )}
-          contentContainerStyle={{ paddingBottom: 20 }}
-        />
-      </VStack>
-    </VStack>
+            <FlatList
+              data={exercises}
+              keyExtractor={(_, index) => String(index)}
+              showsVerticalScrollIndicator={false}
+              renderItem={({ item }) => (
+                <ExerciseCard name={item} onPress={handleOpenExerciseDetails} />
+              )}
+              contentContainerStyle={{ paddingBottom: 20 }}
+            />
+          </VStack>
+        </>
+      )
+      }
+    </VStack >
   )
 }
