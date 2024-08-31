b9dfaa5c4a88c7627367f64e912a5906f8584738
daviaxs <cadwvk@gmail.com>
2024-08-31 12:48:03 -0300

    refactor: Update HomeHeader to display user's name dynamically

--- src/shared/components/home-header/HomeHeader.tsx
+++ src/shared/components/home-header/HomeHeader.tsx
@@ -1,8 +1,11 @@
 import { UserPhoto } from "@components/user-photo/UserPhoto";
 import { Heading, HStack, Icon, Text, VStack } from "@gluestack-ui/themed";
+import { useAuth } from "@hooks/useAuth";
 import { LogOut } from "lucide-react-native";
 
 export function HomeHeader() {
+  const { user } = useAuth()
+
   return (
     <HStack bg="$gray600" pt="$16" pb="$5" px="$8" alignItems="center" gap="$4">
       <UserPhoto
@@ -15,7 +18,7 @@ export function HomeHeader() {
       <VStack flex={1}>
         <Text color="$gray100" fontSize="$sm">Olá</Text>
 
-        <Heading color="$gray100" fontSize="$md">Davi Alves</Heading>
+        <Heading color="$gray100" fontSize="$md">{user.name}</Heading>
       </VStack>
 
       <Icon as={LogOut} color="$gray200" size="xl" />
