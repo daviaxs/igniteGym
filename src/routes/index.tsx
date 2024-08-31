8ea056fbf47306a3e023ba90d67bfbeb3aeaf6ee
daviaxs <cadwvk@gmail.com>
2024-08-31 10:50:24 -0300

    feat: add check if user is logged in to show AppRoutes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,15 +3,18 @@ import { AuthRoutes } from "./auth.routes"
 import { Box } from "@gluestack-ui/themed"
 import { gluestackUIConfig } from "../../config/gluestack-ui.config"
 import { AppRoutes } from "./app.routes"
+import { useAuth } from "@hooks/useAuth"
 
 export function Routes() {
   const theme = DefaultTheme
   theme.colors.background = gluestackUIConfig.tokens.colors.gray700
 
+  const { user } = useAuth()
+
   return (
     <Box flex={1} bg="$gray700">
       <NavigationContainer theme={theme}>
-        <AuthRoutes />
+        {user.id ? <AppRoutes /> : <AuthRoutes />}
       </NavigationContainer>
     </Box>
   )
